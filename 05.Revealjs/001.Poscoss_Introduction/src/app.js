e293e5e7c919eae9acb1ce975e93bc25a662f4b1
KennyNguyen88 <giatrangrua@gmail.com>
2017-11-21 20:16:09 +0700

    [Revealjs] Poscoss_Introduction

---
+++ 05.Revealjs/001.Poscoss_Introduction/src/app.js
@@ -0,0 +1,20 @@
+//Styles
+import "./scss/styles.scss";
+//Images
+import "./images/overseas.png";
+import "./images/domestic.png";
+import "./images/quality.jpg";
+//JS
+require ("./js/head.min.js");
+var Reveal = require ("./js/reveal.js");
+Reveal.initialize({
+    history: true
+});
+
+// require("./js/marked");
+// require("./js/markdown");
+// require("./js/notes");
+// require("./js/zoom");
+// require("./js/highlight");
+// require("./js/classList");
+// require("./js/html5shiv");
