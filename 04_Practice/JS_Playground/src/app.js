31209b6a9b7c2f1a612831753444ab419c9425b4
BlackAnt <giatrangrua@gmail.com>
2017-11-30 16:57:10 +0700

    [PRACTICE] [JS_PLAYGROUND]

---
+++ 04_Practice/JS_Playground/src/app.js
@@ -0,0 +1,92 @@
+// import "./scss/styles.scss";
+// import "bootstrap";
+
+//pop: Remove an item from the end of an array
+//Returns the removed item.
+let cats = ['Bob', 'Willy', 'Mini'];
+console.log("Original", cats);
+let popItem= cats.pop();
+console.log("popItem", popItem);
+console.log("pop", cats);
+
+//push: Add items to the end of an array
+//Returns the new array length
+let lengthAfterPush = cats.push("push");
+console.log("push", cats);
+
+//shift: Remove an item from the beginning of an array
+//Returns the removed item
+let shiftItem = cats.shift();
+console.log("shiftItem", shiftItem);
+console.log("shift", cats);
+
+//unshift: Add items to the beginning of an array
+//Returns the new array length
+let lengthAfterUnshift = cats.unshift("unshift");
+console.log("unshift",cats);
+
+//join: join all elements of an array with separator
+//Returns new string
+let separator = ",";
+let joinArray = cats.join(separator);
+console.log("join", joinArray);
+
+//concat: merge 2 array
+//Returns new array
+let arr1 = ['a', 'b', 'c'];
+let arr2 = ['d', 'a', 'f'];
+let arr3 = arr1.concat(arr2);
+console.log("concat",arr3);
+
+//indexOf: returns the first index (>=0) which given element can be found or -1 if cannot
+let ia = arr3.indexOf("a");
+console.log("indexOf a", ia);
+let ie = arr3.indexOf("e");
+console.log("indexOf e", ie);
+
+//foreach
+//no return
+//cannot break out the loop
+const queues = [1,2,3,4,5];
+queues.forEach((queue,index) => {
+    console.log(queue + " => " + index);
+})
+
+//for...in
+//use with object properties
+//no return
+//cannot break out the loop
+var obj = {a: 1, b: 2, c: 3};
+for (let prop in obj) {
+console.log(`obj.${prop} = ${obj[prop]}`);
+}
+
+//filter
+//return new array
+
+let filter2 = queues.filter(queue => {
+    if (queue !== 2){
+        return true;
+    }
+    return false;
+})
+console.log("filter",filter2);
+
+let filter1 = queues.filter(queue => queue == 1);
+console.log("filter",filter1);
+
+//map: transform array
+//return new array
+
+let mapArray = queues.map(queue => {return ("'" + queue + "'")});
+console.log("map",mapArray);
+
+
+//reduce
+//return only 1 value
+
+let total = queues.reduce((accumulator, currentValue, currentIndex, array) => {
+    return accumulator + currentValue;
+});
+
+console.log("reduce", total);
